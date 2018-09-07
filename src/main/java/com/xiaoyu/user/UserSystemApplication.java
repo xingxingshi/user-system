f4bfb335a4b64b39bd110725374370eca2838df4
hzq <hzq@blibao.com>
2018-09-07 17:30:20 +0800

    Initial commit

---
+++ src/main/java/com/xiaoyu/user/UserSystemApplication.java
@@ -0,0 +1,16 @@
+package com.xiaoyu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class UserSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSystemApplication.class, args);
+    }
+}
+
+
